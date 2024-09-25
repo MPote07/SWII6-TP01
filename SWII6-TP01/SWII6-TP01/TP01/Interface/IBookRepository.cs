fba80f09fc3506a531086cf87c79d71caa20db23
MPote07 <105569325+MPote07@users.noreply.github.com>
2024-09-24 21:10:03 -0300

    Add files via upload

---
+++ SWII6-TP01/SWII6-TP01/TP01/Interface/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP01.Models;
+
+// Lucas Vieira dos Santos Martins
+// Miguel da Silva Pataro
+
+namespace TP01.Interface
+{
+    public interface IBookRepository
+    {
+        ICollection<Book> getAll();
+        void add(Book book);
+    }
+}
